d7c6c0b0bbbc3faaaf0fc4d5c1aa7d877f3ee6de
Getnet Bogale <getnetbogale27@gmail.com>
2024-10-20 12:38:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -325,7 +325,7 @@ import scipy.stats as stats
 
 @st.cache_data
 def load_data():
-    url = 'https://raw.githubusercontent.com/getnetbogale27/Child-Undernutrition-ML-app/refs/heads/master/data/ML-Data.csv?token=GHSAT0AAAAAACY5XTINZM2YD7A7H47DSQ3IZYTUMRQ'
+    url = 'https://raw.githubusercontent.com/getnetbogale27/Child-Undernutrition-ML-app/refs/heads/master/data/ML-Data.csv?token=GHSAT0AAAAAACY5XTIM2JABHYUASMZWHMVEZYU3WTA'
     df = pd.read_csv(url)
     return df
 
